447689023e208dba4875dc0ad7f57a1d1b612393
epicmindwarp <epicmindwarp@gmail.com>
2024-04-16 23:57:19 +0100

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,17 @@
+import { Devvit } from '@devvit/public-api';
+import { settingsForModQScanner, checkModQScannerSubmitEvent } from "./modq_scanner.js";
 
+Devvit.configure({
+  redditAPI: true, // <-- this allows you to interact with Reddit's data api
+});
+
+Devvit.addSettings([
+  settingsForModQScanner
+]);
+
+Devvit.addTrigger({
+  events: ['PostSubmit', 'PostReport', 'PostUpdate', 'PostCreate', 'PostDelete', 'PostFlairUpdate', 'CommentDelete', 'CommentUpdate', 'CommentReport', 'ModAction'],
+  onEvent: checkModQScannerSubmitEvent,
+});
+
+export default Devvit;
---
+++ src/modq_scanner.ts
@@ -0,0 +1,72 @@
+import { PostSubmit, PostReport, PostUpdate, PostCreate, PostDelete, PostFlairUpdate,
+        CommentDelete, CommentUpdate, CommentReport, ModAction} from "@devvit/protos"
+
+import { SettingsFormField, TriggerContext } from "@devvit/public-api";
+
+enum modQScannerSettingName {
+    EnableFeature = "enablemodQScanner",
+}
+
+export const settingsForModQScanner: SettingsFormField = {
+    type: "group",
+    label: "ModQ Scanner",
+    helpText: "Scan the modqueue for reported comments with deleted authors or OPs",
+    fields: [
+        {
+            name: modQScannerSettingName.EnableFeature,
+            type: "boolean",
+            label: "Enable ModQueue Deleted Comment Clean-up",
+            defaultValue: true,
+        },
+  ],
+};
+
+export async function checkModQScannerSubmitEvent (event: PostSubmit | PostReport | PostUpdate |
+                                                            PostCreate | PostDelete | PostFlairUpdate |
+                                                            CommentDelete | CommentUpdate | CommentReport |
+                                                            ModAction
+                                                            , context: TriggerContext)
+ {
+
+    console.log('\nTriggered by: ' + event.type.toString())
+
+    // Do not do anything if turned off
+    if (!modQScannerSettingName.EnableFeature){
+        console.log('ModQ Actions Disabled.')
+        return;
+    }
+
+    const subreddit = await context.reddit.getSubredditById(context.subredditId);
+
+    // Comments only
+    let modq_comments = await subreddit.getModQueue({ type: 'comment' });
+    let modq_comments_count = (await modq_comments.all()).length;
+
+    // If the comment no longer has an OP, delete it
+    // If the parent post of the comment no longer has an OP, delete it
+    console.log('Comments in ModQ: ' + modq_comments_count);
+
+    for (let i = 0; i < modq_comments.children.length; i++) {
+
+        const comment = modq_comments.children[i];
+
+        // Remove comment if the author is deleted
+        if (!comment.authorName){
+            comment.remove()
+            console.log('\tCleared orphaned comment ' + comment.permalink)
+        }
+        else {            
+            // Identify the main post
+            let comment_parent_post = await context.reddit.getPostById(comment.postId)
+
+            // If the post has the author deleted, delete the comment
+            if (!comment_parent_post.authorName){
+                comment.remove()
+                console.log('Cleared orphaned comment (post) ' + comment.permalink)
+            }
+        }
+    }
+
+    console.log('Finished.\n\n')
+
+}
